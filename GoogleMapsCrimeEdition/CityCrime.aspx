63af8de408f0ce5df03ebb336357044e8e98a030
Dan Cundy <Dancundy@hotmail.com>
2017-05-16 20:06:15 +0100

    First Commit

---
+++ GoogleMapsCrimeEdition/CityCrime.aspx
@@ -0,0 +1,160 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CityCrime.aspx.cs" Inherits="GoogleMapsCrimeEdition.CityCrime" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+   <title>GoogleMapsCrimeEdition</title>
+
+    <style>
+        #map {
+            width: 100%;
+            height: 500px;
+            background-color: grey;
+        }
+
+        .inlineblock {
+            display: inline-block;
+        }
+
+        .ctrlwrapper {
+            margin: 5px;
+        }
+
+        #controls {
+            float: left;
+        }
+
+        #headerwrapper {
+            width: 100%;
+        }
+
+     
+        
+    </style>
+
+</head>
+<body>
+    <script>
+
+        function addcrimepoints() {
+
+            var crimejsonasstring = document.getElementById("HiddenField").value;
+
+            if (crimejsonasstring) {
+                debugger;
+                var json = JSON.parse(crimejsonasstring);
+                for (var i = 0; i < json.length; i++) {
+
+                    var point = new mappoint(json[i].CrimeID, json[i].Month, parseFloat(json[i].Latitude),
+                        parseFloat(json[i].Longitude), json[i].Location, json[i].Category, json[i].OutcomeStatus);
+
+                    addpoints(point);
+                }
+            }
+        }
+
+        //Variables.
+        var maplocation;
+        var map;
+        var marker;
+        var infowindow;
+
+        //Map point object.
+        function mappoint(cid, mo, lat, lng, loc, cat, otcm) {
+
+            this.CrimeID = cid,
+            this.Month = mo,
+            this.Latitude = lat,
+            this.Longitude = lng,
+            this.Location = loc,
+            this.Category = cat,
+            this.OutcomeStatus = otcm;
+        }
+
+        //Map Initialisation.
+        function initMap() {
+            maplocation = { lat: 51.4113822, lng: -0.7529996 };
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 13,
+                center: maplocation,
+                mapTypeId: 'roadmap'
+            });
+
+            addcrimepoints();
+        }
+
+
+
+        //Create and add point to google map.
+        function addpoints(point) {
+            debugger;
+
+            var pos = { lat: point.Latitude, lng: point.Longitude };
+            var marker = new window.google.maps.Marker({
+                position: pos,
+                map: map,
+                clickable: true
+            });
+
+            var outcome;
+            if (point.OutcomeStatus === "") {
+                outcome = "Unknown";
+            } else {
+                outcome = point.OutcomeStatus;
+            }
+
+            var contentString =
+                '<div id="content">' +
+                    '<div id="siteNotice">' +
+                    '</div>' +
+                    '<h1 id="firstHeading" class="firstHeading">Crime Infomation</h1>' +
+                    '<div id="bodyContent">' +
+                    '<p><b>CrimeID</b>: ' + point.CrimeID +
+                    '<p><b>Date (yyyy:mm)</b>: ' + point.Month +
+                    '<p><b>Location</b>: ' + point.Location +
+                    '<p><b>Category</b>: ' + point.Category +
+                    '<p><b>Outcome</b>: ' + outcome +
+                    '</p>' +
+                    '<p>Source: <a href="https://data.police.uk/"> https://data.police.uk/ </a>' +
+                    '</p>' +
+                    '</div>' +
+                    '</div>';
+
+
+             infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            marker.addListener('click', function () {
+                infowindow.close();
+                infowindow.open(map, marker);
+            });
+        }
+
+    </script>
+    <form id="form1" runat="server">
+        <h3>Google Crime</h3>
+        <div id="headerwrapper" class="inlineblock">
+         <div id="controls" class="inlineblock">
+                <div id="divtype" class="inlineblock ctrlwrapper">
+                    <p class="inlineblock">Crime Type</p>
+                    <asp:DropDownList runat="server" ID="cmbCrimeType" class="inlineblock">
+                    </asp:DropDownList>
+                </div>
+                <div id="divstatus" class="inlineblock ctrlwrapper">
+                    <p class="inlineblock">Crime Status</p>
+                    <asp:DropDownList runat="server" ID="cmbCrimeStatus" class="inlineblock">
+                    </asp:DropDownList>
+                </div>
+                <div id="divrefresh" class="inlineblock ctrlwrapper">
+                    <button id="btnrefresh" runat="server" type="submit" onserverclick="btnrefresh_OnServerClick">Refresh</button>
+                </div>
+            </div>
+        </div>
+        <asp:HiddenField ID="HiddenField" ClientIDMode="Static" runat="server" />
+        <div id="map"></div>
+        <script async defer type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_b-D0gd79AhyosCOwJvR_iPYtR-l7aHI&callback=initMap"></script>
+    </form>
+</body>
+</html>
---
+++ GoogleMapsCrimeEdition/CityCrime.aspx.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using Newtonsoft.Json;
+
+namespace GoogleMapsCrimeEdition
+{
+    public partial class CityCrime : System.Web.UI.Page
+    {
+        private const string Path =
+             (@"C:\Users\DanCu\OneDrive\Documents\Visual Studio 2015\WebSites\GoogleMapPlotTest\CSVFiles\5b743788-99b9-4814-8f29-3cf75735023e.csv");
+
+        /// <summary>
+        /// Point object used to contain longitude and latitude on a map.
+        /// </summary>
+        public class Point
+        {
+            /// <summary>
+            /// Latitude of map point
+            /// </summary>
+            public string Lat { get; set; }
+
+            /// <summary>
+            /// Longitute of map point
+            /// </summary>
+            public string Log { get; set; }
+        }
+
+        /// <summary>
+        /// List of map points 
+        /// </summary>
+        public List<Point> MapPoints { get; set; }
+
+        /// <summary>
+        /// CSV crime object.
+        /// Maps to row in CSV file.
+        /// </summary>
+        public class Crime
+        {
+            public string CrimeId { get; set; }
+            public string Month { get; set; }
+            public string Latitude { get; set; }
+            public string Longitude { get; set; }
+            public string Location { get; set; }
+            public string Category { get; set; }
+            public string OutcomeStatus { get; set; }
+        }
+
+        /// <summary>
+        /// List of map points 
+        /// </summary>
+        public List<Crime> Crimes { get; set; }
+
+
+        /// <summary>
+        /// Page load event.
+        /// </summary>
+        private void Page_Load()
+        {
+            if (!Page.IsPostBack)
+            {
+                List<Crime> lstCrimeCsv = GetCrimes(Path);
+                BindComboBoxes(lstCrimeCsv);
+            }
+        }
+
+        /// <summary>s
+        /// Gets crimes from CSV file and returns them 
+        /// as a list of T as well as sends them back to client side via hidden field.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private List<Crime> GetCrimes(string path)
+        {
+            List<Crime> crimes = ReadCsv(path);
+            HiddenField.Value = SerialiseList(crimes);
+
+            return crimes;
+        }
+
+        private void BindComboBoxes(List<Crime> crimes)
+        {
+            List<Crime> distinctTypes = crimes.GroupBy(type => type.Category)
+                .Select(grp => grp.First())
+                .ToList();
+
+            var crimeTypes = distinctTypes.Select(c => c.Category)
+                .Where(c => c != "")
+                .ToList();
+
+            cmbCrimeType.DataSource = crimeTypes;
+            cmbCrimeType.DataBind();
+            cmbCrimeType.Items.Insert(0, "All Crime");
+
+            List<Crime> distinctStatuses = crimes.GroupBy(type => type.OutcomeStatus)
+                .Select(grp => grp.First())
+                .ToList();
+
+            var crimeStatus = distinctStatuses.Select(c => c.OutcomeStatus)
+                .Where(c => c != "")
+                .ToList();
+
+            cmbCrimeStatus.DataSource = crimeStatus;
+            cmbCrimeStatus.DataBind();
+            cmbCrimeStatus.Items.Insert(0, "All Statuses");
+        }
+
+        protected void btnrefresh_OnServerClick(object sender, EventArgs e)
+        {
+            List<Crime> crimes = ReadCsv(Path);
+            List<Crime> filteredResult = crimes;
+
+            string crimeStatusFilter = cmbCrimeStatus.Text;
+            string crimeTypeFilter = cmbCrimeType.Text;
+
+            if (cmbCrimeStatus.SelectedIndex != 0)
+            {
+                filteredResult =
+                    crimes.Where(p => crimes.All(t => p.OutcomeStatus.Contains(crimeStatusFilter))).ToList();
+            }
+            if (cmbCrimeType.SelectedIndex != 0)
+            {
+                filteredResult = filteredResult.Where(p => crimeTypeFilter.All(t => p.Category.Contains(t))).ToList();
+            }
+
+            HiddenField.Value = SerialiseList(filteredResult);
+        }
+
+        /// <summary>
+        /// Serialise lise to string.
+        /// </summary>
+        /// <param name="lstCrimeCsv"></param>
+        /// <returns></returns>
+        private string SerialiseList(List<Crime> lstCrimeCsv)
+        {
+            return JsonConvert.SerializeObject(lstCrimeCsv);
+        }
+
+        /// <summary>
+        /// Reads CSV file using CsvHelper.
+        /// Ref:https://joshclose.github.io/CsvHelper/
+        /// </summary>
+        /// <param name="path"></param>
+        private List<Crime> ReadCsv(string path)
+        {
+            List<Crime> crimes = new List<Crime>();
+
+            TextReader reader = File.OpenText(path);
+            var csv = new CsvReader(reader);
+            while (csv.Read())
+            {
+                Crime crime = new Crime();
+                crime.CrimeId = csv.GetField<string>("Crime ID");
+                crime.Month = csv.GetField<string>("Month");
+                crime.Latitude = csv.GetField<string>("Latitude");
+                crime.Longitude = csv.GetField<string>("Longitude");
+                crime.Location = csv.GetField<string>("Location");
+                crime.Category = csv.GetField<string>("Category");
+                crime.OutcomeStatus = csv.GetField<string>("Outcome status");
+                crimes.Add(crime);
+            }
+
+            return crimes;
+        }
+    }
+}
